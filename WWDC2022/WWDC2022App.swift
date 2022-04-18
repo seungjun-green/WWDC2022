7ddb39eddf16fb52abbe593147333cdf444e6147
seungjun-green <lsj3285007@gmail.com>
2022-04-18 23:18:05 +0900

    Initial Commit

---
+++ WWDC2022/WWDC2022App.swift
@@ -0,0 +1,17 @@
+//
+//  WWDC2022App.swift
+//  WWDC2022
+//
+//  Created by SeungJun Lee on 4/18/22.
+//
+
+import SwiftUI
+
+@main
+struct WWDC2022App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
