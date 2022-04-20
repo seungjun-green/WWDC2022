b5d2a81dab0c1076a9331273711f684bc9b00806
seungjun-green <lsj3285007@gmail.com>
2022-04-20 23:55:25 +0900

    OK, we're making some progress

--- WWDC2022/View/FaceLineView.swift
+++ WWDC2022/View/FaceLineView.swift
@@ -10,7 +10,6 @@ import SwiftUI
 struct FaceLineView: View {
     var body: some View {
         Circle().stroke(style: StrokeStyle(lineWidth: 3, lineCap: .square,  dash: [12], dashPhase:2))
-            .frame(width: 250, height: 250)
             .foregroundColor(Color.blue)
             .glow(color: .blue, radius: 12)
     }
