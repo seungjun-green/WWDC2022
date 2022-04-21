cbfb07bb963e8f539239266f81cf4bbd5a20d629
seungjun-green <lsj3285007@gmail.com>
2022-04-22 04:36:50 +0900

    updated master data

--- WWDC2022/Data/MasterData.swift
+++ WWDC2022/Data/MasterData.swift
@@ -17,16 +17,23 @@ class DataR {
             "patterns": ["bye", "see you", "have a nice day"],
             "responses": ["Bye", "Ok, See you then", "It was nice meeting you", "Take care", "Goodbye for now", "I'll see you later"]
         ],
-        "question":[
-            "patterns": ["Who are you", "Why are you here", "What is your age", "Who's your creator?", "Where are you from?"],
-            "responses": ["Bye", "Ok, See you then", "It was nice meeting you", "Take care", "Goodbye for now", "I'll see you later"]
-        ],
         "name": [
-            "patterns": ["What's your name?", "What is your name?", "Your Name?", "WHat should I call you?"],
-            "responses":["I'm XE42", "YOu can call me XE42","I'm XE42"]
+            "patterns": ["What's your name?", "What is your name?", "Your Name?", "What should I call you?"],
+            "responses":["I'm XE42", "You can call me XE42","I'm XE42"]
+        ],
+        "time": [
+            "patterns": ["time?","What time is it now", "Current time?", "Can you tell me the time, please?", "I would like to know the time.", "Is it time for lunch yet?"],
+            "responses": ["Hmmmm, It's always time for launch somewhere in this Earth"]
+        ],
+        "weather": [
+            "patterns": ["weather?","What is the current weather like?", "What's the weather like right now?", "Is it raining outside?", "What's the temperature right now?"],
+            "responses": ["Sorry, I also don't know what time it is", "Hmm, I guess time for snacks?"]
+        ],
+        "age": [
+            "patterns": ["How old are you?","What is your age?", "I would like to know your age.", "Could you tell me your age?", "May I ask your age?", "Can I ask how old you are?"],
+            "responses": ["I'm 1 month old", "I stopped counting my age from some point of my life"]
         ]
     ]
-    
 }
 
 
