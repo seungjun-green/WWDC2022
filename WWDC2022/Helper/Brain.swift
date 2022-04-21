4bcf3e53b99c07be3db2c9fc807aedc4c1dab79d
seungjun-green <lsj3285007@gmail.com>
2022-04-22 04:10:53 +0900

    FUckkkk yeah!!!!, not it's working!!!!!

--- WWDC2022/Helper/Brain.swift
+++ WWDC2022/Helper/Brain.swift
@@ -19,36 +19,52 @@ class Brain {
         let processedString = input.trimmingCharacters(in: .whitespacesAndNewlines)
         
         if processedString == "" {
-            // if human said nothing
+            /// if human said nothing
             return "U gotta say something"
         } else {
-            if processedString.starts(with: "Remember that") {
+            if processedString.starts(with: "Remember that") || processedString.starts(with: "Remember") {
+                /// time to remember something
                 if processedString.count == "Remember that".count {
+                    /// human only said remember that
                     return "Remember what?, Could you say that again?"
                 } else {
-                    // do proecess of saving
-                    // get the questiona nd answer form then string
-                    //save it to the dictionariy
-                    let question = ""
+                    /// human correctly used remember that phrase
+                    
+                    // from given input , fet the question and asnwer
+                    let question = "What is my name"
                     let answer = "1234"
+                                        
+                    let questions = DataQ.question.keys
                     // if similar question already exist in the question list, then replace that one with a new answer
+                    for curr_q in questions {
+                        print("hahahahaahahah")
+                        if getSimilarity(str1: question, str2: curr_q) >= 0.9 {
+                            print(">>>>>>", getSimilarity(str1: question, str2: curr_q))
+                            DataQ.question[curr_q] = "answer"
+                            return "Ok, I'll remember that from now on"
+                        }
+                        break
+                    }
+                    
+                    // if similar question does not exist, just add one more thing to rememeber
                     DataQ.question[question] = answer
                     print(DataQ.question)
                     return "Ok, I'll remember that from now on"
                 }
             } else {
+                print("Whattttt????---")
+                /// when human said normally, first check if human asked things human made the bot to remember
                 
                 // check if input say is in question list
                     let questions = DataQ.question.keys
                     for curr_q in questions {
-                        print("------------", getSimilarity(str1: processedString, str2: curr_q))
                         if getSimilarity(str1: processedString, str2: curr_q) >= 0.7 {
                             return DataQ.question[curr_q] ?? "I don't know"
                         }
                         break
                     }
                     
-                    // if human said not in Data
+                    /// human was just chlling out, no problem :)
                     do {
                         let config = MLModelConfiguration()
                         let model = try TagClassifier1(configuration: config)
