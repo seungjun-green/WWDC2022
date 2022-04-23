0acebf755e049cbf5e9d6cc7312b8007980705f6
seungjun-green <lsj3285007@gmail.com>
2022-04-24 05:07:34 +0900

    hmm close to final

--- WWDC2022/View/TutorialView.swift
+++ WWDC2022/View/TutorialView.swift
@@ -15,25 +15,41 @@ struct TutorialView: View {
             ScrollView{
                 VStack(alignment: .leading) {
                     Text("Things to test").font(.largeTitle).fontWeight(.medium)
-            }
+                }
                 
                 VStack(alignment: .leading){
-                    Text("Making bot to remember something").font(.headline).fontWeight(.bold)
+                    Text("1.Making bot to remember something").font(.headline).fontWeight(.bold)
                     Text("\n")
                     Text("Start your sentence with 'Remember that'\n")
+                    
+                    Text("EXAMPLE:").fontWeight(.bold)
                     Text("Say 'Remember that my macbook password is 1234'\nSay 'Remember thay my iPhone password is 9876'\n\nThen Ask 'What is my macbook password'\nAsk 'What is my iPhone password'")
                 }
                 
-                Text("\n\n\n")
+                Text("\n\n")
+                
+                
+                
+                
                 
                 VStack(alignment: .leading){
-                    Text("Updating saved information").font(.headline).fontWeight(.bold)
+                    Text("2.Updating saved information").font(.headline).fontWeight(.bold)
                     Text("\n")
+                    Text("EXAMPLE:").fontWeight(.bold)
                     Text("Say 'Remember that my macbook password is abcd'\nThen ask again 'What is my macbook password'")
                 }
                 
-            }.padding(.horizontal)
-                .toolbar {
+                Text("\n\n")
+                
+                
+                
+                VStack(alignment: .leading){
+                    Text("3.Normal Conversation").font(.headline).fontWeight(.bold)
+                    Text("\n")
+                    Text("EXAMPLE:").fontWeight(.bold)
+                    Text("Say 'hi' or 'What is the current time? and so on\n")
+                }
+            }.toolbar {
                     ToolbarItemGroup(placement: .navigationBarLeading) {
                         Button("Ok") {
                             dismiss()
