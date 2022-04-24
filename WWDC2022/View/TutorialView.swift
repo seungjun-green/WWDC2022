ffc397079306fd5b2eadccd316dd6c3ec08bdf24
seungjun-green <lsj3285007@gmail.com>
2022-04-24 23:01:03 +0900

    This is a last commit of wwdc2022 submission app project

--- WWDC2022/View/TutorialView.swift
+++ WWDC2022/View/TutorialView.swift
@@ -13,42 +13,39 @@ struct TutorialView: View {
     var body: some View {
         NavigationView{
             ScrollView{
-                VStack(alignment: .leading) {
-                    Text("Things to test").font(.largeTitle).fontWeight(.medium)
-                }
+               
                 
                 VStack(alignment: .leading){
+                    
+                    Text("Things to test").font(.largeTitle).fontWeight(.medium)
+                    
+                    Text("")
+                    Group{
                     Text("1.Making bot to remember something").font(.headline).fontWeight(.bold)
-                    Text("\n")
+                    Text("")
                     Text("Start your sentence with 'Remember that'\n")
                     
                     Text("EXAMPLE:").fontWeight(.bold)
                     Text("Say 'Remember that my macbook password is 1234'\nSay 'Remember thay my iPhone password is 9876'\n\nThen Ask 'What is my macbook password'\nAsk 'What is my iPhone password'")
-                }
-                
-                Text("\n\n")
-                
-                
-                
-                
-                
-                VStack(alignment: .leading){
-                    Text("2.Updating saved information").font(.headline).fontWeight(.bold)
+                    }
                     Text("\n")
+                    Group{
+                    Text("2.Updating saved information").font(.headline).fontWeight(.bold)
+                    Text("")
                     Text("EXAMPLE:").fontWeight(.bold)
                     Text("Say 'Remember that my macbook password is abcd'\nThen ask again 'What is my macbook password'")
-                }
-                
-                Text("\n\n")
-                
-                
-                
-                VStack(alignment: .leading){
-                    Text("3.Normal Conversation").font(.headline).fontWeight(.bold)
+                    
+                    }
                     Text("\n")
+                    
+                    Group{
+                    Text("3.Normal Conversation").font(.headline).fontWeight(.bold)
+                    Text("")
                     Text("EXAMPLE:").fontWeight(.bold)
                     Text("Say 'hi' or 'What is the current time? and so on\n")
-                }
+                    }
+                }.padding(.horizontal)
+            
             }.toolbar {
                     ToolbarItemGroup(placement: .navigationBarLeading) {
                         Button("Ok") {
